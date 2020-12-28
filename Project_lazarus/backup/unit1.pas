6aa7576a86179a472a10e319e2f04e365410a99c
SudatiSimone <39488019+SudatiSimone@users.noreply.github.com>
2020-12-28 11:19:02 +0100

    upload

---
+++ Project_lazarus/backup/unit1.pas
@@ -0,0 +1,77 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+	Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+	Menus, Buttons, ValEdit, Grids, ExtCtrls;
+
+type
+
+	{ TForm1 }
+
+ TForm1 = class(TForm)
+		btnCalculate: TButton;
+		btnInsert2: TButton;
+		btnInsert3: TButton;
+		btnInsert1: TButton;
+		cmbBox1: TComboBox;
+		cmbBox2: TComboBox;
+		cmbBox3: TComboBox;
+		edtExpression: TEdit;
+		edtResult: TEdit;
+		StaticText1: TStaticText;
+		StaticText2: TStaticText;
+		StaticText3: TStaticText;
+		StaticText4: TStaticText;
+		StaticText5: TStaticText;
+		procedure btnCalculateClick(Sender: TObject);
+  procedure btnInsert1Click(Sender: TObject);
+		procedure btnInsert2Click(Sender: TObject);
+		procedure btnInsert3Click(Sender: TObject);
+
+
+	private
+
+	public
+
+	end;
+
+var
+	Form1: TForm1;
+	Expression: String;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+
+procedure TForm1.btnInsert1Click(Sender: TObject);
+begin
+	edtExpression.Caption := edtExpression.Text + cmbBox1.Text;
+
+end;
+
+
+
+procedure TForm1.btnInsert2Click(Sender: TObject);
+begin
+	edtExpression.Caption := edtExpression.Text +cmbBox2.Text;
+
+end;
+
+procedure TForm1.btnInsert3Click(Sender: TObject);
+begin
+	edtExpression.Caption := edtExpression.Text +cmbBox3.Text;
+end;
+
+procedure TForm1.btnCalculateClick(Sender: TObject);
+begin
+	edtResult.Caption := edtExpression.Caption;
+end;
+
+end.
